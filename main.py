78843ccb08696238770c1925710a9c2bbd57c789
saadwitha <vsaadwitha@gmail.com>
2026-01-07 15:46:00 +0530

    changes

--- main.py
+++ main.py
@@ -1,43 +1,63 @@
-from fastapi import FastAPI, Request
-# from fastapi.responses import HTMLResponse, JSONResponse, RedirectResponse
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 from supabase import create_client
-
-db_url = "https://bkldnlrrpconcjuqkhja.supabase.co"
-db_key = "sb_publishable_2EEWlonB1GPZDCcKT_vM0A_TbIMpl8A"
-
-db = create_client(db_url, db_key)
-
-app = FastAPI()
-
-
-@app.post('/add/contact')
-async def add_contact(request : Request):
-    data = await request.json()
-    db.table('contacts').insert(data).execute()
-    return {"message":"successfull"}
-
-
-@app.get('/contacts')
-def get_all_contacts():
-    result = db.table('contacts').select('*').execute()
-    # contacts = result.data
-    return result.data
-
-
-@app.get('/contact/{contact_id}')
-def get_contact(contact_id: int):
-    result = db.table('contacts').select('*').eq('id', contact_id).execute()
-    # contacts = result.data
-    return result.data
-
-@app.put('/contact/{contact_id}')
-async def add_contact(request: Request,contact_id):
-    data = await request.json()
-    result = db.table('contacts').update(data).eq('id', contact_id).execute()
-    return "updated successfully"
-
-
-@app.delete('/contact/{contact_id}')
-def delete_contacts(contact_id):
-    result = db.table('contacts').delete().eq('id', contact_id).execute()
-    return "deleted successfully"
+ 
+db = create_client("https://lxnfwmegsjrrpkmwamuo.supabase.co", "sb_publishable_LCidkXiw0sqgLPCoQkq32w_Mze1A6rK")
+ 
+app = FastAPI(title= "MET Bank")
+ 
+@app.get('/accounts')
+def get_all_accounts():
+    result = db.table("accounts").select('*').execute()
+    data = result.data
+    print(data)
+    if data:
+        return JSONResponse(data)
+ 
+@app.get('/accoounts/{account_no}')
+def get_account_by_account_number(account_no):
+    result = db.table('accounts').select('*').eq('account_no', account_no).execute()
+    data = result.data
+    if data:
+        return JSONResponse(data)
+ 
+#Transactions
+# /transactions?source=125678212&dest=237568919&amount=500 POST
+ 
+@app.get('/transactions')
+def get_all_transactions():
+    r = db.table('transactions').select('*').execute()
+    d = r.data
+    return JSONResponse(d)
+ 
+@app.post('/transactions')
+def perform_transaction(source, dest, amount):
+    # First see enough balance in source account
+    amount = int(amount)
+    result = db.table('accounts').select('balance').eq('account_no', source).execute()
+    data = result.data
+    src_balance = data[0]['balance']
+ 
+    if src_balance > int(amount):
+        # Decuct balance in source account - record transaction as debit
+        r1 = db.table('accounts').update({'balance': int(src_balance) - int(amount)}).eq('account_no', source).execute()
+        db.table('transactions').insert({
+            'amount': amount,
+            'type': 'debit',
+            'account_no': source
+        }).execute()
+ 
+        # Credit balance in dest account - record transactin as credit
+        r3 = db.table('accounts').select('balance').eq('account_no', dest).execute()
+        d = r3.data
+        dest_balance = d[0]['balance']
+        db.table('accounts').update({'balance': int(dest_balance) + int(amount)}).eq('account_no', dest).execute()
+        db.table('transactions').insert({
+            'amount': amount,
+            'type': 'credit',
+            'account_no': dest
+        }).execute()
+        return JSONResponse({'message': 'Transfer successfull'})
+        # success
+    else:
+        return JSONResponse({'message': 'No Enough balance'})
